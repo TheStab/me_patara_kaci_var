d0c722c5e3b8ffe4836b0f95a352c3f921b239ce
Stab <vruadze@yahoo.com>
2022-08-27 20:34:12 +0400

    first version

---
+++ README.md
@@ -0,0 +1,7 @@
+# MakeupClub
+![home](https://user-images.githubusercontent.com/58137357/144869058-5f5d3615-0e04-4e5e-8ec9-a38a4faea1cb.jpeg)
+![load](https://user-images.githubusercontent.com/58137357/144869127-58a484c8-46e4-4684-be59-9c7802ebeff5.jpeg)
+![details](https://user-images.githubusercontent.com/58137357/144869136-5bdfc197-5728-45cb-9764-7d213784cc8f.jpeg)
+![product](https://user-images.githubusercontent.com/58137357/144869138-19379ee6-e46b-472e-9953-58c02a747e72.jpeg)
+![success](https://user-images.githubusercontent.com/58137357/144869147-4fa982bc-fd24-4162-9eb4-9ddb5e1833d4.jpeg)
+![profile](https://user-images.githubusercontent.com/58137357/144869151-99f9936e-8c41-4dea-b401-2553c1084cae.jpeg)
---
+++ app/src/main/java/com/deepshikhayadav/makeupclub/MainActivity2.kt
@@ -0,0 +1,20 @@
+package com.deepshikhayadav.makeupclub
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
+      }
+
+}
